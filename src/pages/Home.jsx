8d5187f3c30514763e123657e682152a5068e1c7
Usuário <juliavianamendes05@gmail.com>
2023-03-19 20:31:08 -0300

    adiciona botão que muda para a rota /editToken

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,43 @@ import AddToken from './AddToken';
 export default function Home() {
   const history = useNavigate();
 
-  function handleClick() {
+  function addClick() {
     history('addToken')
+  };
+
+  function editClick() {
+    history('editToken')
   };
 
   return (
     <div className="bg-purple-900 min-h-screen flex items-center justify-center">
       <Header />
-    <button onClick={handleClick}>Add Token</button>
+    <button onClick={addClick}>Add Token</button>
+
+    <table>
+      <thead>
+      <button onClick={editClick}>Editar</button>
+        <tr>Tokens</tr>
+        <tr>Balance</tr>
+      </thead>
+      <tbody>
+        <tr>
+          <button onClick={editClick}>Editar</button>
+          <td>KLV</td>
+          <td>10,250.50</td>
+        </tr>
+        <tr>
+          <td>DVK</td>
+          <td>50,250.71</td>
+        </tr>
+        <tr>
+        <button onClick={editClick}>Editar</button>
+          <td>KFI</td>
+          <td>10</td>
+        </tr>
+      </tbody>
+
+    </table>
     </div>
 
   )
